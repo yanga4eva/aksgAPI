39e356e7c29fbf6f9a2d341b06984f9885c29160
Fortunatus Inyang <juniorinyang@yahoo.co.uk>
2020-04-04 15:48:29 -0600

    ..

--- Route/person.js
+++ Route/person.js
@@ -22,21 +22,20 @@ router.post('/', (req, res) => {
     const saveUser = person.save(function (err, user) {
         if (err) {return console.error(err)
         } else {
-            res.send("Saved")
+            res.json(user)
         }
     })
 })
 
 router.get('/',   async (req, res) => {
 
-    const getUser = person.find(function (err, user){
+    const getUser = await person.find(function (err, user){
         if (err) {return console.error(err)
         } else {
             res.json(user)
         }
     })
     
-    res.send('<body><form autocomplete="off" method="POST" action="/person" class="form-inline my-2 my-lg-0" style="float: right; z-index: -1;" ><input class="form-control mr-sm-2" type="text" placeholder="firstname" aria-label="Search" id="verses" name="firstname" value= ""><input class="btn btn-secondary my-2 my-sm-0" type="submit" value="Submit"></form></body>')
     
 })
 
