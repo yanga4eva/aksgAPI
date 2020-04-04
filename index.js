4daa25dd035b67b406e424e25ce936e910a3e068
Fortunatus Inyang <juniorinyang@yahoo.co.uk>
2020-04-04 12:27:49 -0600

    ..

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const Express = require('express')
 const Mongoose = require('mongoose')
 const BodyParser = require('body-parser')
+const dotenv = require('dotenv')
 const Port = process.env.PORT || 8000
+const path = require('path')
+
+dotenv.config()
+
+// Connect to DB
+Mongoose.connect(
+    process.env.DB_CONNECT,
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},
+() => console.log("DB Connected"));
 
 const app = Express()
+
+// View Enger
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(Express.static(path.resolve('./views')));
+
 
 app.use(BodyParser.json())
 app.use(BodyParser.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {
 
-    res.send("Propsed Akwa Ibom State Digital Project")
+    res.render("index")
 })
 
 app.listen(Port, () => {
