e6da1f0efc5712b0d0a6552f45cbf605c70014f1
Fortunatus Inyang <juniorinyang@yahoo.co.uk>
2020-04-05 09:45:55 -0600

    ..

--- Route/person.js
+++ Route/person.js
@@ -29,7 +29,7 @@ router.post('/', (req, res) => {
 
 router.get('/findperson', (req, res) => {
     var findparams = {'firstname':'okpokam'}
-    var datafilter = ['firstname lastname']
+    var datafilter = 'firstname lastname'
 
     const getUser =  Person.findOne(findparams, datafilter, function (err, result) {
         if (err) {return console.error(err)
