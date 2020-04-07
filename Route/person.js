5decd94a77044ed58de59fdb4e66255aaf41f3a8
Fortunatus Inyang <juniorinyang@yahoo.co.uk>
2020-04-07 04:08:57 -0600

    ..

--- Route/person.js
+++ Route/person.js
@@ -5,7 +5,7 @@ const Food = require('../models/food')
 router.get('/food', (req, res) => {
     var country = req.body.country
 
-    const getUser =  Food.findOne({}, function (err, result) {
+    const getUser =  Food.findOne(country, function (err, result) {
         if (err) {return console.error(err)
         } else {
             res.json(result)
